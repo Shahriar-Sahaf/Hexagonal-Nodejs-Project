6b512726e8adbaf4861737b7e60a8b6a507bbd78
Shahriar <sahafshahriar@gmail.com>
2025-08-21 12:38:06 +0330

    Add Inbound Port For Project

--- src/core/ports/project.service.port.ts
+++ src/core/ports/project.service.port.ts
@@ -2,7 +2,7 @@ import {Project} from '../domain/project'
 
 export interface IProjectServicePort {
     createProject(name: string , author:string , description:string): Promise<Project>;
-    updateProject( name: string , author:string , description:string): Promise<Project>;
+    updateProject(id:number, name: string , author:string , description:string): Promise<Project | null>;
     deleteProject(id: number): Promise<void>;
     findProjectById(id: number): Promise<Project | null>;
     listProject(): Promise<Project[]>;
