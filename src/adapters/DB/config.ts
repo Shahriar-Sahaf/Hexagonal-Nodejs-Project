3ac2ee1fb58b3de7c8f92e6c20e99a3c07c188ad
Shahriar <sahafshahriar@gmail.com>
2025-08-21 12:37:03 +0330

    add Projects Table

--- src/adapters/DB/config.ts
+++ src/adapters/DB/config.ts
@@ -10,5 +10,18 @@ const pool: Pool = new pg.Pool({
     port: 5432,
 });
 
+export const createTableProject = async () => {
+    const query = `
+        CREATE TABLE IF NOT EXISTS projects(
+            id SERIAL PRIMARY KEY,
+            name VARCHAR(255) NOT NULL,
+            author VARCHAR(255) NOT NULL,
+            description TEXT NOT NULL,
+            created_at TIMESTAMP DEFAULT NOW(),
+            updated_at TIMESTAMP DEFAULT NOW()
+        )
+    `;
+    await pool.query(query);
+};
 
 export default pool;
